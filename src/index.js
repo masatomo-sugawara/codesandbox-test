38777deaf7f8dda0528f26309c87b793f9ffd6f2
masatomo-sugawara <masatomo.s1982@gmail.com>
2022-06-22 08:08:58 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -59,13 +59,39 @@
 // }
 // console.log(func1("func1です。"));
 
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です。"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です。"));
 
-// アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です。"));
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 30));
+
+// 分割代入
+// const myPlofile = {
+//   name: "まさとも",
+//   age: 39
+// };
+
+// //const message1 = `名前は${myPlofile.name}です。年齢は${myPlofile.age}歳です。`;
+// //console.log(message1);
+
+// const { name, age } = myPlofile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+const myPlofile = [`まさとも`, 39];
+const message3 = `名前は${myPlofile[0]}です。年齢は${myPlofile[1]}歳です。`;
+//console.log(message3);
+
+const [name, age] = myPlofile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
